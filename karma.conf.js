a9eb8ab4c489a10b18550af01a04fb31b10c8ace
ElMabre <mguzmanarza@gmail.com>
2025-12-12 21:14:50 +0000

    Se agregaron nuevamente dependencias para test

--- karma.conf.js
+++ karma.conf.js
@@ -1,37 +1,50 @@
-// karma.conf.js
-const webpackConfig = require('./webpack.config.test');
+var webpackConfig = require('./webpack.config.test.js');
 
 module.exports = function(config) {
   config.set({
-    frameworks: ['jasmine'],
+    // Frameworks que estás usando
+    frameworks: ['jasmine', 'webpack'],
 
+    // Archivo de entrada de tus pruebas (según tus logs)
     files: [
       'src/tests.entry.js'
     ],
 
-
+    // Preprocesar con webpack para entender React/JSX
     preprocessors: {
-      'src/tests.entry.js': ['webpack', 'sourcemap']
+      'src/tests.entry.js': ['webpack']
     },
 
-    reporters: ['progress', 'coverage'],
-
-    coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
-    },
-    
-
-    singleRun: true, 
+    // Configuración de Webpack (importada del archivo externo o definida aquí si falla)
     webpack: webpackConfig,
+
     webpackMiddleware: {
-      stats: 'errors-only'
+      noInfo: true
     },
+
+    reporters: ['progress'],
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Chrome'],
-    concurrency: Infinity
+    autoWatch: false, // En servidor CI/CD suele ser false
+    singleRun: true,  // Para que corra una vez y termine (ideal para scripts)
+
+    // --- AQUÍ ESTÁ LA SOLUCIÓN DEL NAVEGADOR ---
+    // Usamos un lanzador personalizado para EC2
+    browsers: ['ChromeHeadlessNoSandbox'],
+
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox', // Vital para correr como root/usuario en Linux
+          '--disable-gpu',
+          '--disable-translate',
+          '--disable-extensions',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    }
   });
 };
